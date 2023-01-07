f06657cce3f59ce6b57cd333754105fd25c5b319
Eduardo <ed_hsmo@hotmail.com>
2023-01-07 20:10:37 -0300

    initial structure with material-ui

---
+++ src/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material"
+import { Roboto } from "@next/font/google"
+
+
+const theme = createTheme({
+  palette:{
+    primary:{
+      main: "#0B41E3"
+    },
+  },
+})
+
+
+export default theme
