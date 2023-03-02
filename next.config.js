08310712b0b0f60cb4bad53eb3bc9737743607df
Eduardo <ed_hsmo@hotmail.com>
2023-03-02 20:42:28 -0300

    Revert "enable cors policy to deploy on vercel"
    This reverts commit ad9d00f69c96d2adfbf8655aebd086f77deb6497.

--- next.config.js
+++ next.config.js
@@ -10,21 +10,7 @@ const nextConfig = {
         pathname: "/ad-space/uploads/**"
       }
     ]
-  },
-  headers : async () => {
-    return [
-      {
-        // matching all API routes
-        source: "https://storage.googleapis.com/ad-space/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
-  } 
+  }
 }
 
 module.exports = nextConfig
